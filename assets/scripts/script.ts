2d8c2b19fbcb946b154565b30149bc9d9e7469ba
Patrick Burris <nchupacabra@gmail.com>
2025-06-25 17:45:59 -0500

    Add a console.log abstraction

--- assets/scripts/script.ts
+++ assets/scripts/script.ts
@@ -1,13 +1,4 @@
 export function onUpdate(dt: f32): void {
-  let x = "Hello World";
-  x += "!!!";
-  logMessage(x);
-}
-
-function logMessage(message: string): void {
-  // Encode as Uint8Array directly
-  let utf8 = String.UTF8.encode(message, true); // Returns ArrayBuffer
-  let view = Uint8Array.wrap(utf8); // Create a typed array view
-  let ptr = view.dataStart; // Correct memory pointer
-  log(<i32>ptr, view.length - 1); // Exclude null terminator
+  const x = "!!!";
+  console.log("Hello ", "World", x);
 }
